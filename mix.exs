c7116cc3790d53a94c962a155fc9c23a8331e162
Sash <sashman90@gmail.com>
2016-12-22 17:00:54 +0000

    Bumped version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ExConsulUrl.Mixfile do
 
   def project do
     [app: :ex_consul_url,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
